fd8613b374321f6c5bf8b0b9da58ddfe2a75dd59
Allo127 <1271310565@qq.com>
2020-08-04 11:12:08 +0800

    添加搜索模块

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
+    <searchbar></searchbar>
     <swipe></swipe>
     <grid></grid>
-    这是首页
+    这是首页1
     <menubar></menubar>
   </div>
 </template>
 
 <script>
   import menubar from '../../components/MenuBar.vue'
+  import searchbar from '../../components/SearchBar.vue'
   import Swipe from './base/Swipe.vue'
   import Grid from './base/Grid.vue'
   export default {
     components: {
+      searchbar,
       menubar,
       Swipe,
       Grid
